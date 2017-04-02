f89f062655c2c4894732dd07a41cf1cab4687670
Alexandr Yakovets <375296904276@mail.ru>
2017-04-02 15:43:22 +0300

    added ability to get all offers

--- GoTo.Service/GoTo.Service/GoTo.Service.Provider/Models/Offer.cs
+++ GoTo.Service/GoTo.Service/GoTo.Service.Provider/Models/Offer.cs
@@ -12,11 +12,24 @@ namespace GoTo.Service.Provider.Models
     {
         public string Title { get; set; }
         public string Description { get; set; }
-
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string AuthorID { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public DateTime MeetDateTime { get; set; }
+        public DateTime CreatedDateTime { get; set; }
         internal Offer(OfferDataModel offerDataModel)
         {
             Title = offerDataModel.Title;
             Description = offerDataModel.Description;
+            Longitude = offerDataModel.Longitude;
+            Latitude = offerDataModel.Latitude;
+            AuthorID = offerDataModel.AuthorID;
+            Status = offerDataModel.Status;
+            Type = offerDataModel.Type;
+            MeetDateTime = offerDataModel.MeetDateTime;
+            CreatedDateTime = offerDataModel.CreatedDateTime;
         }
     }
 }
