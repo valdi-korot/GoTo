a3198f8699b1bc0f34076ddc7aa5eb85c5451e91
valdi-korot <valdi-korot@mail.ru>
2017-04-03 23:37:57 +0300

    code refactor

--- GoTo.Service/GoTo.Service/GoTo.Service.Storage.Intergace/Models/OfferDataModel.cs
+++ GoTo.Service/GoTo.Service/GoTo.Service.Storage.Intergace/Models/OfferDataModel.cs
@@ -3,7 +3,7 @@ namespace GoTo.Service.Storage.Intergace.Models
 {
     public sealed class OfferDataModel
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public double Longitude { get; set; }
